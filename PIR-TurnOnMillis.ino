1d72fbe944286206492d4fad9dd59dff7448deca
Jeremy S Cook <jscook55@gmail.com>
2019-06-14 09:22:12 -0400

    Update PIR-TurnOnMillis.ino

--- PIR-TurnOnMillis.ino
+++ PIR-TurnOnMillis.ino
@@ -2,7 +2,7 @@ const int PIRPin = 2;     // the number of the pushbutton pin
 const int OutPin =  3;      // the number of the LED pin
 int PIRState = 0;         // variable for reading the pushbutton status
 
-unsigned long delayValue = 1800000; //30 minutes = 1800 seconds = 1,800,000
+unsigned long delayValue = 600000; //10 minutes = 600 seconds = 600000 milliseconds
 unsigned long triggerTime = delayValue;  //initially set to delayValue, so doesn't come on automatically
 unsigned long currentTime = millis();
 
@@ -19,15 +19,15 @@ void loop() {
   // read the state of the pushbutton value:
   PIRState = digitalRead(PIRPin);
   currentTime = millis();
-  //if (millis() == 0) {
-    //triggerTime = delayValue; //keeps off when millis() rolls over in ~49 days
-  //}
-  if (PIRState == HIGH) {
-    triggerTime = millis();
-    Serial.print("PIR Triggered, triggerTime reset to: "); Serial.print(triggerTime); Serial.print(" Current time: "); Serial.println(currentTime);
+if ((triggerTime > currentTime) {
+    triggerTime = 0; //handles when currentTime rolls over to zero every ~50 days
   }
-  if ((triggerTime > currentTime) && OutPin == HIGH) {
-    triggerTime = 0;
+  if ((PIRState == HIGH)&&(currentTime > 5000)) { //waits 5 seconds after power on to trigger
+    triggerTime = millis();
+    Serial.print("PIR Triggered, triggerTime reset to: "); 
+    Serial.print(triggerTime); 
+    Serial.print(" Current time: "); 
+    Serial.println(currentTime);
   }
   if ((currentTime - triggerTime) < delayValue) { //10 minutes
     digitalWrite(OutPin, HIGH);
